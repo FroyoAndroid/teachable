d134141a4565a431b4e164e0f115da1867e7cb7a
soumya.ghosh <ghoshsoumya001@gmail.com>
2017-08-22 01:44:26 +0530

    google drive API for uploads and downloads in progress

---
+++ BackEndServer/src/common/auth.js
@@ -0,0 +1,7 @@
+var nconf = require('nconf'), 
+    clientDetails = nconf.get('clinetDetails');
+var AuthorizationManager = function(){
+    function AuthorizationManager(){
+
+    }
+}
---
+++ BackEndServer/src/common/google-drive-config.js
@@ -0,0 +1,14 @@
+function googleDriveManager() {
+    var google = require('googleapis'),
+        urlshortener = google.urlshortener('v1'),
+        params = {
+        shortUrl: 'http://goo.gl/xKbRu3'
+    },
+    getLonUrl = () =>{
+        
+    };
+    this.authenticateGogDrive = () => {
+        //obtain OAuth 2.0 credentials such as a client ID and client secret
+
+    }
+}
