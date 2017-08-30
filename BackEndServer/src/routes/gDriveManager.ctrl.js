c6a106fd2410deb2a2909229b10698165594cab2
soumya.ghosh <soumya.ghosh@learningmate.com>
2017-08-31 01:13:55 +0530

    multiple file upload

--- BackEndServer/src/routes/gDriveManager.ctrl.js
+++ BackEndServer/src/routes/gDriveManager.ctrl.js
@@ -11,56 +11,16 @@ var google = require('googleapis'),
     DbManager = require(global.relSrcPath + "/db/mongooseDbManager.js"),
     tokenModel = require(global.relSrcPath + "/models/token.js"),
     dbMngr = new DbManager('register'),
-    tableName = 'token_tbl';
+    tableName = 'token_tbl',
+    UploadManagerSvc = require('../common/uploadManager.svc.js');
 var GDriveManager = function () {
-    var oAuthToken, oAuthClient,
-        uploadData = (drive, fileDetails, res) => {
-            var reqUrl = drive.files.create(fileDetails,
-                function (err, file) {
-                    if (err) {
-                        // Handle error
-                        console.log(err);
-                        //res.status(400).send('Upload error' + err);
-                    } else {
-                        console.log('File Id: ', file.id);
-                       // res.status(200).send('File successfully uploaded with id:' + file.id)
-                    }
-                });
-            console.info(reqUrl.uri.href)
-        },
-        generateFileDetails = (file, parentPath, parentFolderId) => {
-            var fileMetadata = {
-                    'name': file,
-                    parents: [parentFolderId]
-                },
-                fileExt = path.extname(file),
-                mime = (/\.(dcm|jpg|jpeg|nifti|png)$/i).test(file) ? `image/${fileExt.substring(1)}` : '';
-            var media = {
-                mimeType: mime,
-                body: fs.createReadStream(`${parentPath}/${file}`)
-            };
-            return {
-                resource: fileMetadata,
-                media: media,
-                fields: 'id'
-            };
-        }
+    var oAuthToken, oAuthClient;
 
     function GDriveOAuth() {}
     this.testoAuth = (req, res, next) => {
         var url = authSvc.getAuthUrl();
         res.send(url);
 
-    }
-    this.getCode = (url, req, res, next) => {
-        req.get(url, function (err, res, body) {
-            if (err) {
-                next(err);
-            } else {
-                next(res)
-            }
-
-        });
     }
     this.createToken = function (req, res, next) {
         oAuthClient = oauth2Client = authSvc.getOAuthClient(),
@@ -93,58 +53,36 @@ var GDriveManager = function () {
 
         var oauth2Client = authSvc.getOAuthClient();
         //get token details from cache
-        var fileMetadata = {
-                'name': req.params['filename'],
-                'mimeType': 'application/vnd.google-apps.folder'
-            },
-            parentPath = `../BackEndServer/files/${req.params['filename']}`;
         redis.get('token_abc', function (err, tokens) {
-            if (err) res.status(404).send('token not found');
-            oauth2Client.setCredentials(JSON.parse(tokens));
-            var drive = google.drive({
-                version: 'v3',
-                auth: oauth2Client
-            });
-            var reqUrl = drive.files.create({
-                    resource: fileMetadata,
-                    fields: 'id'
-                },
-                function (err, file) {
-                    if (err) console.error('Upload folder err');
-                    else {
-                        var parentFolderId = file.id;
-                        res.status(200).send('File successfully uploaded with id:' + file.id)
-                        fs_promisified.readdir(parentPath)
-                            .then(files => {
-                                files.forEach((file, index) => {
-                                    var fileDetais = generateFileDetails(file, parentPath, parentFolderId);
-                                    console.info(file);
-                                    uploadData(drive, fileDetais, res)
-                                });
-                            })
-                            .catch(err => {
-                                console.error(err);
-                            });
-                    }
-                });
-            console.info(reqUrl.uri.href)
-
+            if (err || !tokens) res.status(404).send('token not found');
+            else {
+                oauth2Client.setCredentials(JSON.parse(tokens));
+                var uploadMgr = new UploadManagerSvc(google, oauth2Client, fs_promisified, path);
+                uploadMgr.upload(req.params['filename'])
+            }
         });
 
     }
     this.listData = (req, res, next) => {
         var oauth2Client = authSvc.getOAuthClient();
         redis.get('token_abc', function (err, tokens) {
-            if (err) res.status(404).send('token not found');
-            oauth2Client.setCredentials(JSON.parse(tokens));
-            var drive = google.drive({
-                version: 'v3',
-                auth: oauth2Client
-            });
-            drive.files.list("mimeType='image/jpg'", function (data) {
-                console.info(data);
-                if (data === null) res.status(400).send([]);
-            });
+            if (err || !tokens) res.status(404).send('token not found');
+            else {
+                oauth2Client.setCredentials(JSON.parse(tokens));
+                var drive = google.drive({
+                    version: 'v3',
+                    auth: oauth2Client
+                });
+                drive.files.list({
+                    media: {
+                        mimeType: 'application/vnd.google-apps.folder'
+                    }
+                }, function (data) {
+                    console.info(data);
+                    if (data === null) res.status(400).send([]);
+                });
+            }
+
         });
 
     }
